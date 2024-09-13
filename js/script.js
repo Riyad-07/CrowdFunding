66f7ad42bde988d96140ff3badde93ad236802cb
Riyad-07 <samimsamim1998@gmail.com>
2024-09-13 23:32:52 +0600

    Home Page

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll:1,
+    prevArrow: '<i class="fa-solid fa-arrow-left prev"></i>',
+    nextArrow: '<i class="fa-solid fa-arrow-right next"></i>',
+  });
